3289aeeb79a2912a5ee44381674c9866ba041b17
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-04-15 21:54:58 -0600

    validando los datos del formulario

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,7 +1,13 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +19,12 @@ public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
 		model.addAttribute("Titulo", "Formulario usuarios");
+		model.addAttribute("usuario",usuario);
 		return "form";
 	}
-	
+	/*
 	@PostMapping("/form")
 	public String procesar(Model model, 
 			@RequestParam(name="username") String username,
@@ -30,12 +38,31 @@ public class FormController {
 			usuario.setEmail(email);
 			
 			model.addAttribute("titulo", "Resultado Form");
-			/*
 			model.addAttribute("username", username);
 			model.addAttribute("password", password);
 			model.addAttribute("email", email);
-			*/
+			
 			model.addAttribute("usuario",usuario);
+		return "resultado";
+	}*/
+	
+	//Otra forma de poblar los datos 
+	//Los parametros se deben de llamar igual
+	
+	@PostMapping("/form")
+	public String procesar(@Valid Usuario usuario,BindingResult result, Model model) {
+		
+		model.addAttribute("titulo", "Resultado Form");
+			if (result.hasErrors()) {
+				Map<String,String> errores = new HashMap<>();
+				result.getFieldErrors().forEach(err ->{
+					errores.put(err.getField(),"El campo ".concat( err.getField()).concat(" ").concat(err.getDefaultMessage()));
+				});
+				model.addAttribute("error",errores);
+				return "form";
+			}
+			model.addAttribute("usuario",usuario);
+			
 		return "resultado";
 	}
 }
