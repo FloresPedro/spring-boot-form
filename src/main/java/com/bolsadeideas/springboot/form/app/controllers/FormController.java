d3890e29d034e15942217be1229d435ec9c09e06
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-04-15 21:04:10 -0600

    Inicio manejo formularios

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -0,0 +1,29 @@
+package com.bolsadeideas.springboot.form.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FormController {
+
+	@GetMapping("/form")
+	public String form(Model model) {
+		model.addAttribute("Titulo", "Formulario usuarios");
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String procesar(Model model, 
+			@RequestParam(name="username") String username,
+			@RequestParam String password,
+			@RequestParam String email) {
+			model.addAttribute("titulo", "Resultado Form");
+			model.addAttribute("usermane", username);
+			model.addAttribute("password", password);
+			model.addAttribute("email", email);
+		return "Resultado";
+	}
+}
